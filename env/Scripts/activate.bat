c0117265db93a9f61e87ea3965583de4d3414595
Stervar <150718158+Stervar@users.noreply.github.com>
2024-10-21 18:20:29 +0300

    vg

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\279014673\Snake-Termenal-1\env
+set VIRTUAL_ENV=C:\Users\HyperPC\Documents\GitHub\Snake\env
 
 if not defined PROMPT set PROMPT=$P$G
 
